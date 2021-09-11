b0d627d0fb09b548f45893bc3ef131bdc8338b00
joshuagato <joshuagatogato37@gmail.com>
2021-09-11 11:38:39 +0000

    set up for the React root index file with redux, its extensions and redux-notifications

--- src/index.js
+++ src/index.js
@@ -3,11 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import axios from 'axios';
+import thunk from 'redux-thunk';
+import { reducer as notifications } from 'react-notification-system-redux';
+import { productsReducer, archivedProductsReducer } from '../src/store/reducers';
+
+axios.defaults.baseURL = 'http://www.mocky.io/v2/5c3e15e63500006e003e9795';
+
+const rootReducer = combineReducers({
+    productsReducer,
+    archivedProductsReducer,
+    notifications
+});
+
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <Provider store={store}>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </Provider>,
   document.getElementById('root')
 );
 
