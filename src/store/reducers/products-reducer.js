50824e7adfc5bc1631398527416a587bc8444776
joshuagato <joshuagatogato37@gmail.com>
2021-09-14 02:32:37 +0000

    Added caching to products-reducer.js
    Signed-off-by: joshuagato <joshuagatogato37@gmail.com>

--- src/store/reducers/products-reducer.js
+++ src/store/reducers/products-reducer.js
@@ -3,47 +3,81 @@ import {
     DELETE_PRODUCT, DELETE_COMPLETE_RESET, EDIT_PRODUCT, EDIT_PRODUCT_COMPLETE_RESET
 } from '../actions/action-types';
 
-import { sortProductListByIdAscendingOrder } from '../../globals'
+import { sortProductListByIdAscendingOrder } from '../../globals';
+
+const getCachedProductsState = variable => JSON.parse(localStorage.getItem('productsState'))?.[variable];
 
 const initialState = {
-    products: [],
-    addProductComplete: false,
-    deleteProductComplete: false,
-    editProductComplete: false
+    products: getCachedProductsState('products') || [],
+    addProductComplete: getCachedProductsState('addProductComplete') || false,
+    deleteProductComplete: getCachedProductsState('deleteProductComplete') || false,
+    editProductComplete: getCachedProductsState('editProductComplete') || false
 };
 
 const removeProductById = (products, id) => products.filter(product => product.id !== id);
 
 const reducer = (state = initialState, action) => {
+    console.log(getCachedProductsState('products'));
+    let products;
+    let currentState;
     switch (action.type) {
         case SET_INITIAL_PRODUCTS:
-            return { ...state, products: [...new Set(state.products), ...new Set(action.data)] };
+            const initialProducts = [...new Set(state.products), ...new Set(action.data)];
+            products = [...new Set(sortProductListByIdAscendingOrder(initialProducts))];
+            currentState = { ...state, products };
+            localStorage.setItem('productsState', JSON.stringify(currentState));
+            return currentState;
         case ADD_NEW_PRODUCT:
-            return { ...state, products: [...state.products, action.data] };
+            products = [...new Set(sortProductListByIdAscendingOrder([...state.products, action.data]))];
+            currentState = { ...state, products };
+            localStorage.setItem('productsState', JSON.stringify(currentState));
+            return currentState;
         case ADD_PRODUCT_COMPLETE:
-            return { ...state, addProductComplete: action.addProductComplete };
+            currentState = { ...state, addProductComplete: action.addProductComplete };
+            localStorage.setItem('productsState', JSON.stringify(currentState));
+            return currentState;
         case ADD_PRODUCT_COMPLETE_RESET:
-            return { ...state, addProductComplete: action.addProductComplete };
+            currentState = { ...state, addProductComplete: action.addProductComplete };
+            localStorage.setItem('productsState', JSON.stringify(currentState));
+            return currentState;
         case EDIT_PRODUCT:
             const oldProducts = [...state.products];
             const newProducts = (removeProductById(state.products, action.data.id));
             const finalProducts = [...newProducts, action.data];
+
             if (oldProducts.length !== newProducts.length &&
-                newProducts.length === oldProducts.length - 1)
-                return { ...state, products: [...sortProductListByIdAscendingOrder(finalProducts)], editProductComplete: true };
+                newProducts.length === oldProducts.length - 1) {
+                products = [...sortProductListByIdAscendingOrder(finalProducts)];
+                currentState = { ...state, products, editProductComplete: true };
+                localStorage.setItem('productsState', JSON.stringify(currentState));
+                return currentState;
+            }
+
+            localStorage.setItem('productsState', JSON.stringify(state));
             return state;
         case EDIT_PRODUCT_COMPLETE_RESET:
-            return { ...state, editProductComplete: action.editProductComplete };
+            currentState = { ...state, editProductComplete: action.editProductComplete };
+            localStorage.setItem('productsState', JSON.stringify(currentState));
+            return currentState;
         case DELETE_PRODUCT:
             const oldProductsList = [...state.products];
             const newProductsList = (removeProductById(state.products, action.id));
+
             if (oldProductsList.length !== newProductsList.length &&
-                newProductsList.length === oldProductsList.length - 1)
-                return { ...state, products: [...newProductsList], deleteProductComplete: true };
+                newProductsList.length === oldProductsList.length - 1) {
+                currentState = { ...state, products: [...sortProductListByIdAscendingOrder(newProductsList)], deleteProductComplete: true };
+                localStorage.setItem('productsState', JSON.stringify(currentState));
+                return currentState;
+            }
+
+            localStorage.setItem('productsState', JSON.stringify(state));
             return state;
         case DELETE_COMPLETE_RESET:
-            return { ...state, deleteProductComplete: action.deleteProductComplete };
+            currentState = { ...state, deleteProductComplete: action.deleteProductComplete };
+            localStorage.setItem('productsState', JSON.stringify(currentState));
+            return currentState;
         default:
+            localStorage.setItem('productsState', JSON.stringify(state));
             return state;
     }
 };
