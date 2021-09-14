434abc12c306b6d6d015fe5bb5c13ea99f9c6320
joshuagato <joshuagatogato37@gmail.com>
2021-09-14 02:35:42 +0000

    Added exporters for all external action creators
    Signed-off-by: joshuagato <joshuagatogato37@gmail.com>

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,4 +1,5 @@
 export { fetchInitialProducts, addNewProduct, addProductCompleteReset, deleteProduct,
-    deleteProductCompleteReset, editProduct, editProductCompleteReset } from './products-actions';
+    deleteProductCompleteReset, editProduct, editProductCompleteReset,
+    restoreOldProduct } from './products-actions';
 export { addNewProductToArchive, restoreArchivedProduct,
     addToArchiveCompleteReset, restoreFromArchiveCompleteReset } from './archived-products-actions';
