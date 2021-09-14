33ee3ed7921734db8735b5d5726f6edc272d0218
joshuagato <joshuagatogato37@gmail.com>
2021-09-14 02:32:25 +0000

    Added caching to archived-products-reducer.js
    Signed-off-by: joshuagato <joshuagatogato37@gmail.com>

--- src/store/reducers/archived-products-reducer.js
+++ src/store/reducers/archived-products-reducer.js
@@ -1,25 +1,43 @@
-import { ADD_NEW_PRODUCT_TO_ARCHIVE, RESTORE_ARCHIVED_PRODUCT } from '../actions/action-types';
+import { ADD_NEW_PRODUCT_TO_ARCHIVE, RESTORE_ARCHIVED_PRODUCT,
+    RESTORE_FROM_ARCHIVE_COMPLETE_RESET } from '../actions/action-types';
+import { sortProductListByIdAscendingOrder } from '../../globals';
+
+const getCachedArchivedProductsState = variable => JSON.parse(localStorage.getItem('archivedProductsState'))?.[variable];
 
 const initialState = {
-    archivedProducts: [],
-    addToArchiveComplete: false,
-    restoreFromArchiveComplete: false
+    archivedProducts: getCachedArchivedProductsState('archivedProducts') || [],
+    addToArchiveComplete: getCachedArchivedProductsState('addToArchiveComplete') || false,
+    restoreFromArchiveComplete: getCachedArchivedProductsState('restoreFromArchiveComplete') || false
 };
 
 const removeProductById = (products, id) => products.filter(product => product.id !== id);
 
 const reducer = (state = initialState, action) => {
+    let currentState;
     switch (action.type) {
         case ADD_NEW_PRODUCT_TO_ARCHIVE:
-            return { ...state, archivedProducts: [...state.archivedProducts, action.data], addToArchiveComplete: true };
+            const newArchiveProductsList = [...state.archivedProducts, action.data];
+            currentState = { ...state, addToArchiveComplete: true,
+                archivedProducts: [...new Set(sortProductListByIdAscendingOrder(newArchiveProductsList))] };
+            localStorage.setItem('archivedProductsState', JSON.stringify(currentState));
+            return currentState;
         case RESTORE_ARCHIVED_PRODUCT:
             const oldProductsList = [...state.archivedProducts];
             const newProductsList = (removeProductById(state.archivedProducts, action.id));
             if (oldProductsList.length !== newProductsList.length &&
-                newProductsList.length === oldProductsList.length - 1)
-                return { ...state, archivedProducts: [...newProductsList], restoreFromArchiveComplete: true };
-            else return state;
+                newProductsList.length === oldProductsList.length - 1) {
+                currentState = { ...state, archivedProducts: [...sortProductListByIdAscendingOrder(newProductsList)], restoreFromArchiveComplete: true };
+                localStorage.setItem('archivedProductsState', JSON.stringify(currentState));
+                return currentState;
+            }
+            localStorage.setItem('archivedProductsState', JSON.stringify(state));
+            return state;
+        case RESTORE_FROM_ARCHIVE_COMPLETE_RESET:
+            currentState = { ...state, restoreFromArchiveComplete: action.restoreFromArchiveComplete };
+            localStorage.setItem('archivedProductsState', JSON.stringify(currentState));
+            return currentState;
         default:
+            localStorage.setItem('archivedProductsState', JSON.stringify(state));
             return state;
     }
 };
